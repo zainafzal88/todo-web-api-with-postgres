98fb06f92c8fe015923d86548e636e1bf768a6ae
Zain Afzal <zain.afzal@rise.cx>
2020-05-24 10:31:12 +1000

    web api reading from postgres

---
+++ README.md
@@ -0,0 +1,10 @@
+# todo-web-api
+Create a .NET Core Web API and connect it to PostgreSQL using C#.
+
+## Getting Started
+1.  Familiarity with C#, Model-View-Controller (MVC) and Entity Framework
+2.  Visual Studio For Mac
+3.  Postman
+
+## Resource
+1.  [Microsoft Tutorial](https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio-mac)
---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
